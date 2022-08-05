99cc03590a4da94408eff775bb251fbd69c7926d
Raul Alejandro Salas Texido <alejandrosalas0891@gmail.com>
2022-08-05 13:39:16 +0200

    [ADD] Implement Custom Hook Test

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,13 +1,49 @@
 import { render, screen } from "@testing-library/react"
 import { GifGrid } from "../../src/components"
+import { useFetchGifs } from "../../src/hooks/useFetchGifs"
+
+jest.mock('../../src/hooks/useFetchGifs')
 
 describe('Test in <GifGrid/>', () => {
     const category = 'naruto'
-    test('', () => {
+    test('Should display the loading and initial category ', () => {
+        
+        //Initialize useFetchGifs hook
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: true //for loading text
+        })
+        
         render(<GifGrid category={category}/>)
         //screen.debug()
         expect(screen.getByText('Loading ...'))
         expect(screen.getByText( category ))
      })
+
+     test('should display images items when loaded using useFetchGifs', () => { 
+        
+        const gifs= [
+            {
+                id:'1',
+                title: 'Saitama',
+                url: 'htpps://saitama.com/saitama.jpg'
+            },
+            {
+                id:'2',
+                title: 'Naruto',
+                url: 'htpps://naruto-web.com/naruto.jpg'
+            }
+        ]
+
+        useFetchGifs.mockReturnValue({
+            images:gifs,
+            isLoading: false
+        })
+        
+        render(<GifGrid category={category}/>)    
+        
+        expect(screen.getAllByRole('img').length).toBe(2)
+        //screen.debug()
+      })
 
 })
---
+++ tests/hooks/useFetchGif.test.js
@@ -0,0 +1,26 @@
+import { renderHook, waitFor } from "@testing-library/react"
+import { useFetchGifs } from "../../src/hooks/useFetchGifs"
+
+
+
+describe('Test in Hook useFetchGif', () => { 
+
+    test('should return the initial state', () => {
+
+       const { result } = renderHook(()=> useFetchGifs('One Punch'))
+       const { images,isLoading } = result.current
+       expect( images.length).toBe(0)
+       expect( isLoading ).toBeTruthy()
+    })
+
+    test('should return an array of images and variable isLoading is false', async() => { 
+        const { result } = renderHook(()=> useFetchGifs('One Punch'))
+
+        await waitFor(()=> expect(result.current.images.length).toBeGreaterThan(0))
+
+       const { images,isLoading } = result.current
+       expect( images.length).toBeGreaterThan(0)
+       expect( isLoading ).toBeFalsy()
+     })
+
+ })
